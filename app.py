f76ff69cff87555a428f07912cbcfa5d2c8ac993
Dev H <devhotchandani5@gmail.com>
2025-02-08 09:53:11 +0530

    version 2.0.0

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/products")
+def products():
+    return "<p>This is the products page</p>"
+
+@app.route("/about")
+def about():
+    return "<p>Learn more about us here!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
